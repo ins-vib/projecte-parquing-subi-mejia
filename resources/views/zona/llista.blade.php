35b9d14e105fa1c3afe744bf06fa5c39235e0b78
joan <orsu423@vidalibarraquer.net>
2024-12-18 09:27:59 +0100

    .

--- resources/views/zona/llista.blade.php
+++ resources/views/zona/llista.blade.php
@@ -1,24 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Parkings') }}
+            Detalls de la {{$zonas->nom}}
         </h2>
     </x-slot>
-
-<h2>Detalls de la {{$zonas->nom}}</h2>
 
 <table>
     <tr>
         <td><strong>Id</strong> </td>
         <td>{{$zonas->id}}</td>
     </tr>  
-    <tr>
-        <td><strong>Nom</strong> </td>
-        <td>{{$zonas->nom}}</td>
-    </tr>
-    <tr>
-        <td><strong>Estat</strong> </td>
-        <td>{{$zonas->nom}}</td>
-    </tr>    
 </table>
 </x-app-layout>
