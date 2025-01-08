34496ce592abb32440a038baa2812e78c7c85253
Oriol <152179454+SubiMinion@users.noreply.github.com>
2025-01-08 17:18:26 +0100

    view aparcar

--- app/Http/Controllers/AparcarController.php
+++ app/Http/Controllers/AparcarController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Parking;
 
 class AparcarController extends Controller
 {
     //
 
     public function aparcar() {
-        return view('aparcar.aparcar');
+        $parkings = Parking::all();
+        return view('aparcar.aparcar')->with('parkings', $parkings);
     }
 }
