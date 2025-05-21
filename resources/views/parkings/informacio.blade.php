05cf7cbca353e216d1bec4c0400c60c1b7173cc9
Oriol Subiros <orsu423@vidalibarraquer.net>
2025-05-21 11:46:34 +0200

    rellotge a totes les views

--- resources/views/parkings/informacio.blade.php
+++ resources/views/parkings/informacio.blade.php
@@ -4,6 +4,18 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             Detalls de {{$parkings->name}}
         </h2>
+        <h1>
+            <div id="clock" style="color: rgb(134, 125, 125); text-align: left;"></div>
+            <script>
+                function updateClock() {
+                    let options = { timeZone: 'Europe/Madrid', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+                    let now = new Date().toLocaleTimeString('ca-ES', options);
+                    document.getElementById('clock').textContent = now;
+                }
+                setInterval(updateClock, 1000);
+                updateClock();
+            </script>
+        </h1>
     </x-slot>
 
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg" style="margin: 1%">
